6c899d6f2c3592febbcf1a13486ec899bd1b1089
ppedraum <pedrovirtuozo@gmail.com>
2021-08-16 17:24:30 -0300

    exercicios
    Fazendo os exercícios 1, 2 e 3.

---
+++ 2021-08-16/ex3.py
@@ -0,0 +1,5 @@
+dia = input("Digite o dia de seu nascimento: ")
+mes = input("Digite o mês de seu nascimento: ")
+ano = input("Digite o ano de seu nascimento: ")
+
+print("Nascimento: " + dia + "/" + mes + "/" + ano)
